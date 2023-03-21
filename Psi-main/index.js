509be3d7710af97f9f7dcee67fa66ccf21e310b8
Pe9cha <120149308+Pe9cha@users.noreply.github.com>
2023-03-21 14:30:09 +0700

    Update index.js

--- Psi-main/index.js
+++ Psi-main/index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const express = require("express");
-const record = require('node-record-lpcm16');
 var bodyParser = require('body-parser');
 const base64ToImage = require('base64-to-image');
 const { Writable } = require('stream');
